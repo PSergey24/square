6d7eeebe65454c9a049c8771fc179dcd2392b31b
Vladimir Ilin <xrigikx@mail.ru>
2016-10-10 20:32:37 +0300

    Clone yii2/basic. Delete custom controllers, views, models.

---
+++ controllers/IndexController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace app\controllers;
+
+use Yii;
+use yii\web\Controller;
+
+class IndexController extends Controller
+{
+
+    public function actionIndex()
+    {
+    }
+
+    public function actionRegister()
+    {
+
+    }
+
+    public function actionAuth()
+    {
+
+    }
+
+    public function actionAbout()
+    {
+
+    }
+
+    public function actionGames()
+    {
+
+    }
+
+    public function actionArea()
+    {
+
+    }
+}
