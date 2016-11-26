754a936ee80b558561077a13c380480a1c28f7a9
Nikolay Golos <Nikolay Golos>
2016-11-26 23:39:54 +0300

    games.css created games, changed registartion page

--- views/game/index.php
+++ views/game/index.php
@@ -9,9 +9,191 @@ use yii\widgets\Pjax;
 $this->title = 'Games';
 $this->registerCssFile('/css/games.css');
 $this->params['breadcrumbs'][] = $this->title;
+$this->registerJsFile('https://maps.googleapis.com/maps/api/js?key=AIzaSyDxkhFJ3y--2AadULGUoE9kdlRH3nT-668&callback=initMap',
+    [
+        'async' => true,
+    ]
+);
+$this->registerJs("
+    var map, myloc_marker, myloc_infoWindow, infowindow, contentString;
+    var markers = new Array();
+    var directionsDisplay, directionsService, map;
+    function initMap() {
+        var directionsService = new google.maps.DirectionsService();
+        directionsDisplay = new google.maps.DirectionsRenderer();
+        var latlng = new google.maps.LatLng(59.910326, 30.3185942);
+        var options = {
+            zoom: 11,
+            center: latlng,
+            mapTypeControl: true,
+            mapTypeControlOptions: {
+                style: google.maps.MapTypeControlStyle.DROPDOWN_MENU
+            },
+            navigationControl: true,
+            mapTypeId: google.maps.MapTypeId.ROADMAP,
+            zoomControl: true,
+            scaleControl: true
+        };
+
+        map = new google.maps.Map(document.getElementById('map'), options);
+        directionsDisplay.setMap(map);
+        
+        if (navigator.geolocation) {
+          navigator.geolocation.getCurrentPosition(function(position) {
+            var pos = {
+              lat: position.coords.latitude,
+              lng: position.coords.longitude
+            };
+
+            myloc_marker = new google.maps.Marker({
+                position: pos,
+                map: map,
+                icon: '/img/my_location.png'
+            });
+            myloc_infoWindow = new google.maps.InfoWindow({
+                content: 'Вы находитесь здесь'
+            });
+            myloc_infoWindow.open(map, myloc_marker);
+            map.setCenter(pos);
+            map.setZoom(14);
+          }, function() {
+          });
+        } else {
+          // Browser doesn't support Geolocation
+          alert('Ошибка: Ваш браузер не поддерживает геолокацию!');
+        }           
+    }
+", $this::POS_HEAD);
 ?>
-<div class="game-index">
-    <i class="fa fa-cog fa-spin fa-5x fa-fw"></i><i class="fa fa-cog fa-spin fa-5x fa-fw"></i>
-    <h1 class="h2-black">Данная страница<br>находится в процессе разработки</h1>
-    <a href="#" onclick="history.back();" class="button">Назад</a>
-</div>
+
+    <div class="container-fluid info">
+    	<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 forSmall" id="small">
+    		<div class="col-lg-4 col-md-5 col-sm-6 col-xs-12 games">
+    			<div class="search">Поиск игр <a href="#">Ближайшие к вам</a></div>
+                <h1>Раздел в стадии разработки</h1>
+    			<div class="game">
+                    <div class="description">
+                        <p class="adress">Соболевская ул. д 45 (Лучше обходить сбоку)</p>
+                        <i class="fa fa-futbol-o" aria-hidden="true" style="color:#F44336;" title="Нужен мяч"></i>
+                        <p class="sport">Баскетбол</p>
+                        <p class="buttons">
+                            <a href="#" class="mid-blue-btn hideXS"> На карте</a>
+                            <a href="#" class="mid-blue-btn showXS"> На площадку</a>
+                            <a href="#" class="mid-green-btn">Игроков <span>+9</span></a>
+                        </p>
+                    </div>
+                    <div class="time">
+                        <div class="align">сегодня<br><span class="date">22:48</span></div>
+                    </div>         
+                </div>
+                <div class="game">
+                    <div class="description">
+                        <p class="adress">Соболевская ул. д 45 (Лучше обходить сбоку)</p>
+                        <i class="fa fa-futbol-o" aria-hidden="true" style="color:#F44336;" title="Нужен мяч"></i>
+                        <p class="sport">Баскетбол</p>
+                        <p class="buttons">
+                            <a href="#" class="mid-blue-btn hideXS"> На карте</a>
+                            <a href="#" class="mid-blue-btn showXS"> На площадку</a>
+                            <a href="#" class="mid-green-btn">Игроков <span>+9</span></a>
+                        </p>
+                    </div>
+                    <div class="time">
+                        <div class="align">сегодня<br><span class="date">22:48</span></div>
+                    </div>         
+                </div>
+                <div class="game">
+                    <div class="description">
+                        <p class="adress">Соболевская ул. д 45 (Лучше обходить сбоку)</p>
+                        <i class="fa fa-futbol-o" aria-hidden="true" style="color:#F44336;" title="Нужен мяч"></i>
+                        <p class="sport">Баскетбол</p>
+                        <p class="buttons">
+                            <a href="#" class="mid-blue-btn hideXS"> На карте</a>
+                            <a href="#" class="mid-blue-btn showXS"> На площадку</a>
+                            <a href="#" class="mid-green-btn">Игроков <span>+9</span></a>
+                        </p>
+                    </div>
+                    <div class="time">
+                        <div class="align">сегодня<br><span class="date">22:48</span></div>
+                    </div>         
+                </div>
+                <div class="game">
+                    <div class="description">
+                        <p class="adress">Соболевская ул. д 45 (Лучше обходить сбоку)</p>
+                        <i class="fa fa-futbol-o" aria-hidden="true" style="color:#F44336;" title="Нужен мяч"></i>
+                        <p class="sport">Баскетбол</p>
+                        <p class="buttons">
+                            <a href="#" class="mid-blue-btn hideXS"> На карте</a>
+                            <a href="#" class="mid-blue-btn showXS"> На площадку</a>
+                            <a href="#" class="mid-green-btn">Игроков <span>+9</span></a>
+                        </p>
+                    </div>
+                    <div class="time">
+                        <div class="align">сегодня<br><span class="date">22:48</span></div>
+                    </div>         
+                </div>
+                <div class="game">
+                    <div class="description">
+                        <p class="adress">Соболевская ул. д 45 (Лучше обходить сбоку)</p>
+                        <i class="fa fa-futbol-o" aria-hidden="true" style="color:#F44336;" title="Нужен мяч"></i>
+                        <p class="sport">Баскетбол</p>
+                        <p class="buttons">
+                            <a href="#" class="mid-blue-btn hideXS"> На карте</a>
+                            <a href="#" class="mid-blue-btn showXS"> На площадку</a>
+                            <a href="#" class="mid-green-btn">Игроков <span>+9</span></a>
+                        </p>
+                    </div>
+                    <div class="time">
+                        <div class="align">сегодня<br><span class="date">22:48</span></div>
+                    </div>         
+                </div>
+                <div class="game">
+                    <div class="description">
+                        <p class="adress">Соболевская ул. д 45 (Лучше обходить сбоку)</p>
+                        <i class="fa fa-futbol-o" aria-hidden="true" style="color:#F44336;" title="Нужен мяч"></i>
+                        <p class="sport">Баскетбол</p>
+                        <p class="buttons">
+                            <a href="#" class="mid-blue-btn hideXS"> На карте</a>
+                            <a href="#" class="mid-blue-btn showXS"> На площадку</a>
+                            <a href="#" class="mid-green-btn">Игроков <span>+9</span></a>
+                        </p>
+                    </div>
+                    <div class="time">
+                        <div class="align">сегодня<br><span class="date">22:48</span></div>
+                    </div>         
+                </div>
+                <div class="game">
+                    <div class="description">
+                        <p class="adress">Соболевская ул. д 45 (Лучше обходить сбоку)</p>
+                        <i class="fa fa-futbol-o" aria-hidden="true" style="color:#F44336;" title="Нужен мяч"></i>
+                        <p class="sport">Баскетбол</p>
+                        <p class="buttons">
+                            <a href="#" class="mid-blue-btn hideXS"> На карте</a>
+                            <a href="#" class="mid-blue-btn showXS"> На площадку</a>
+                            <a href="#" class="mid-green-btn">Игроков <span>+9</span></a>
+                        </p>
+                    </div>
+                    <div class="time">
+                        <div class="align">сегодня<br><span class="date">22:48</span></div>
+                    </div>         
+                </div>
+                <div class="game">
+                    <div class="description">
+                        <p class="adress">Соболевская ул. д 45 (Лучше обходить сбоку)</p>
+                        <i class="fa fa-futbol-o" aria-hidden="true" style="color:#F44336;" title="Нужен мяч"></i>
+                        <p class="sport">Баскетбол</p>
+                        <p class="buttons">
+                            <a href="#" class="mid-blue-btn hideXS"> На карте</a>
+                            <a href="#" class="mid-blue-btn showXS"> На площадку</a>
+                            <a href="#" class="mid-green-btn">Игроков <span>+9</span></a>
+                        </p>
+                    </div>
+                    <div class="time">
+                        <div class="align">сегодня<br><span class="date">22:48</span></div>
+                    </div>         
+                </div>
+                
+    		</div>
+    		<div class="col-lg-8 col-md-7 col-sm-6 col-xs-12" id="map"></div>
+    	</div>
+    </div>
+
--- views/user/security/login.php
+++ views/user/security/login.php
@@ -13,6 +13,7 @@ use yii\widgets\ActiveForm;
  */
 
 $this->title = Yii::t('user', 'Регистрация/Авторизация');
+$this->registerCssFile('/css/reg.css');
 ?>
 
 <div id="container-form" class="col-lg-offset-4 col-lg-4 col-md-offset-4 col-md-4 col-sm-offset-3 col-sm-6 col-xs-12">
