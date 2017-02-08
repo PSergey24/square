86e51fc310ecf8446c731e5c552efadfd746eb41
PSergey24 <sproshchenko@yandex.ru>
2017-02-08 14:17:11 +0300

    users map and game

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -119,6 +119,7 @@ class SiteController extends Controller
                         ->where(['game_id' => $gameItem['id']])
                         ->andWhere('game_user.user_id = profile.user_id');
                     $players = $queryPlayer->orderBy('game_user.id desc')->all();
+                    $games[$i]['count'] = count($players);
                     array_push($users,$players);
                     $plus = '+';
                     foreach ($players as $player) {
@@ -150,6 +151,7 @@ class SiteController extends Controller
                         ->andWhere('game_user.user_id = profile.user_id');
                     $players = $queryPlayer->orderBy('game_user.id desc')->all();
 
+                    $games2[$i]['count'] = count($players);
                     $flag = 0;
                     foreach ($players as $player) {
                         if($player['user_id'] == $userAuth)
@@ -193,11 +195,20 @@ class SiteController extends Controller
         $query = new Query();
         $query_games = new Query();
 
-        $query->select('court.id as id, address')
+        // получаем id авторизованного пользователя
+            if(Yii::$app->user->identity)
+                $userAuth = Yii::$app->user->identity->getId();
+            else
+                $userAuth = 0;
+
+        $query->select('court.id as id, address,type_id,lat,lon')
             ->from('court_bookmark, court')
             ->where(['user_id' => $id])
             ->andWhere('court_id = court.id');
         $courts = $query->all();
+
+        
+
         $photo = [];
         $i=0;
         foreach ($courts as $court) {
@@ -210,35 +221,57 @@ class SiteController extends Controller
             if($query->one())
             {
                 $photo[$i] = $query->one();
+                $courts[$i]['photo'] = $photo[$i]; 
             }
             else
+            {
                 $photo[$i]['photo'] = 'defaultCourt.jpg';
+                $courts[$i]['photo'] = 'defaultCourt.jpg'; 
+            }
+
+            $queryCount = new Query();
+            $queryCount->select('user_id')
+                ->from('court_bookmark')
+                ->where(['court_id' => $court['id']]);
+            $countPl = $queryCount->all();
+            $courts[$i]['count'] = count($countPl);
+
             $i++;
         }
 
         $games = [];
+        $users = [];
 
-            $query_games->select('court.id as court_id, address, time, need_ball')
-                ->from('court,game,game_user')
+            $query_games->select('game.id as id,court.id as court_id, address, time, sport_type.name as sport, need_ball,sport_type_id')
+                ->from('court,game,game_user,sport_type')
                 ->where(['user_id' => $id])
                 ->andWhere('game_id = game.id')
                 ->andWhere('court_id = court.id')
                 ->andWhere(['>=', 'time',date('Y-m-d H:i:s', strtotime(date('Y-m-d H:i:s')))])
+                ->andWhere('sport_type_id = sport_type.id')
+                ->groupBy('game.id')
                 ->orderBy('time');
-            $game_rows = $query_games->all();
+            $games = $query_games->all();
 
-            foreach ($game_rows as $row) {
-                $tm = strtotime($row['time']);
-                $current_datetime = new DateTime();
-                $current_datetime = date_format($current_datetime, 'Y-m-d');
-                $tm_current = strtotime($current_datetime);
-                if (date("d", $tm) == date("d", $tm_current))
-                    $row['time'] = 'Сегодня ' . date("H:i", $tm);
-                elseif(date("d", $tm) == date(date("d")+1, $tm_current))
-                    $row['time'] = 'Завтра ' . date("H:i", $tm);
-                else
-                    $row['time'] = date("d.m.Y", $tm) ." ".date("H:i", $tm);
-                array_push($games, $row);
+            $i = 0;
+            foreach ($games as $row) {
+
+                $queryPlayer = new Query;
+                    $queryPlayer->select('game_user.user_id as user_id, picture')
+                        ->from('game_user, profile')
+                        ->where(['game_id' => $row['id']])
+                        ->andWhere('game_user.user_id = profile.user_id');
+                    $players = $queryPlayer->orderBy('game_user.id desc')->all();
+                    array_push($users,$players);
+                    $games[$i]['count'] = count($players);
+                    $plus = '+';
+                    foreach ($players as $player) {
+                        if($player['user_id'] == $userAuth)
+                            $plus = '-';
+                    }
+                    $games[$i]['plus'] = $plus;
+
+                $i++;
             }
 
         $query->select('picture')
@@ -249,6 +282,7 @@ class SiteController extends Controller
         return $this->render('users.php', [
             'courts' => $courts,
             'games' => $games,
+            'users' => $users,
             'username' => User::find('username')->where(['id' => $id])->one()->username,
             'picture' => $picture,
             'photo' => $photo,
--- views/site/users.php
+++ views/site/users.php
@@ -12,6 +12,159 @@ $this->registerCssFile('/css/userProfile.css',[
 $this->registerCssFile('/css/squareProfile.css',[
     'depends' => [AppAsset::className()]
 ]);
+
+$this->registerJsFile('https://maps.googleapis.com/maps/api/js?key=AIzaSyDxkhFJ3y--2AadULGUoE9kdlRH3nT-668&callback=initMap',
+    [
+        'async' => true,
+        'defer' => true
+    ]
+);
+$this->registerJs("
+    var map, myloc_marker, myloc_infoWindow, infowindow, contentString;
+    var markers = new Array();
+    var SPB_lat = 59.910326;
+    var SPB_lng = 30.3185942;
+    var directionsDisplay, directionsService, map;
+    function initMap() {
+        var directionsService = new google.maps.DirectionsService();
+        directionsDisplay = new google.maps.DirectionsRenderer();
+        var latlng = new google.maps.LatLng(SPB_lat, SPB_lng);
+        var options = {
+            zoom: 9,
+            center: latlng,
+            mapTypeControl: false,
+            mapTypeControlOptions: {
+                style: google.maps.MapTypeControlStyle.DROPDOWN_MENU
+            },
+            navigationControl: true,
+            mapTypeId: google.maps.MapTypeId.ROADMAP,
+            zoomControl: true,
+            scaleControl: false,
+            streetViewControl:false
+        };
+
+        map = new google.maps.Map(document.getElementById('map'), options);
+        directionsDisplay.setMap(map);
+
+        
+            infowindow = new google.maps.InfoWindow({
+                content: contentString
+            });
+
+            var result = ".json_encode($courts).";
+            var photo = ".json_encode($photo).";
+
+            $.each(result, function (index, value) {
+                
+                if(value['type_id'] == 1){
+                    var pinImgLink = '/img/basket.png';
+                    var styleItem = 'basketball';
+                }else if((value['type_id'] == 2)){
+                    var pinImgLink = '/img/foot.png';
+                    var styleItem = 'football';
+                }
+                else if((value['type_id'] == 3)){
+                    var pinImgLink = '/img/volleyball.png';
+                    var styleItem = 'volleyball';
+                }else{
+                    var pinImgLink = '/img/other.png';
+                    var styleItem = '';
+                }
+
+        
+                var marker = new google.maps.Marker({
+                    id: value['id'],
+                    position: {lat: Number(value['lat']), lng: Number(value['lon'])},
+                    map: map,
+                    animation: google.maps.Animation.DROP,
+                    style: styleItem,
+                    court: value['court_id'],
+                    address: value['address'],
+                    visible: true,
+                    photo: photo[index]['photo'],
+                    bookmark: value['count'],
+                    icon: pinImgLink
+                });
+
+                google.maps.event.addListener(marker, 'click', function() {
+                    infowindow.close();
+                    if(myloc_infoWindow) { 
+                        myloc_infoWindow.close(); 
+                    }
+                    var contentString = '<div class=\"searchImgForm court_info\">' +
+                                            '<div class=\"forSmall\">' +
+                                                '<a href=\"/court/' + this.id + 
+                                                '\"><div style=\"background-image: url(/img/courts/' + this.photo + ');\" class=\"image-right image\">' +
+                                                '<div class=\"close\"></div><div class=\"players\">' +
+                                                    '<i class=\"fa fa-male\" aria-hidden=\"true\"></i>'+ this.bookmark +'</div><span>Открыть площадку</span></div>' +
+                                                '<div class=\"sliderText center \">' + this.address + '</div></a>' +
+                                            '</div>' +
+                                        '</div>';
+
+
+// var contentString = '<div class=\"searchImgForm court_info\">' +
+
+//                                         '</div>';
+
+                    infowindow.setContent(contentString);
+                    infowindow.open(map, marker);
+                });
+
+
+
+
+                markers.push(marker);
+            })
+
+    
+    }
+", $this::POS_HEAD);
+
+$this->registerJs("
+    function people(game,symbol){
+
+        $.ajax({
+            type: 'POST',
+            url: '/game/player',
+            data: 'game='+game+'&&symbol='+symbol,
+            success: function(data){
+                if(data == 'Вы не авторизованы')
+                    alert(data);
+                else{
+                    var result = data.split('|');
+                    if(result[1] == 0)
+                    {
+                        $('[data-id-game='+result[0]+']').remove();
+                        $.pjax.reload({container: \"#gamesPjax\"});
+                    }
+                    else{
+                        $('[data-id-game='+result[0]+'] .circle a').remove();
+                        $('[data-id-game='+result[0]+'] .playersMap a').remove();
+                        if(result[2] == '-')
+                        {
+                            $('[data-id-game-plus='+result[0]+'] span').html('+');
+                            $('[data-id-game-plus='+result[0]+']').attr('onclick','people('+result[0]+',\'+\')');
+                        }
+                        else{
+                            $('[data-id-game-plus='+result[0]+'] span').html('-');
+                            $('[data-id-game-plus='+result[0]+']').attr('onclick','people('+result[0]+',\'-\')');
+                        }
+                        $('[data-id-game='+result[0]+'] .count').html(result[1]);
+                        
+                        
+                        $('[data-id-game='+result[0]+'] .circle').append(result[3]);
+                        
+                    }
+                }
+            },
+            error:  function(data){
+                    alert('Ошибка: '+data);
+            }
+        });
+    }
+
+", $this::POS_HEAD);
+
 if(Yii::$app->user->identity)
     $userAuth = Yii::$app->user->identity->getId();
 else
@@ -48,23 +201,49 @@ else
         <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12 usersSquares">
             <div class="box usersSquare con col-lg-12 col-md-12 col-sm-12 col-xs-12 shadow">
                 <div class="header"><div class="menu">Площадки <?= $username ?><span> <?= count($courts) ?></span></div></div>
-                <div class="col-lg-12 col-xs-12 col-md-12 col-sm-12 col-xs-12" id="map">
-                    Я карта с метками площадок пользователя, если карты нет, то заглушка, она закомменчена снизу
-                </div>
-<!--                 <div class="noinfo">
+                <?php 
+                if(count($courts) > 0)
+                {
+                ?>
+                <div class="col-lg-12 col-xs-12 col-md-12 col-sm-12 col-xs-12" id="map"></div>
+                <?php 
+                }else{
+                ?>
+                <div class="noinfo">
                     <i class="fa fa-street-view fa-2x blue" aria-hidden="true"></i>
                     <br><?= $username ?> еще добавил <br class="hidden-xs"> в свой профиль площадки<br>
-                </div> -->
+                </div>
+                <?php 
+                }
+                ?>
             </div>
 
         </div>
         <div class="gamesWrap col-lg-offset-1 col-lg-4 col-md-offset-1 col-md-5 col-sm-6 col-xs-12">
             <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 box games shadow" id="game_list">
                     <div class="header"><div class="menu">Игры <?= $username ?></div></div>
+                    <?php 
+                    if(count($games) > 0)
+                    {
+                        $n = 0;
+                        foreach ($games as $game) { $n++;?>
+                            <?= $this->render('_game_profile', [
+                                'n' => $n,
+                                'game' => $game,
+                                'users' => $users
+                            ]) ?>
+
+                    <?php 
+                        }
+                    }else{
+                    ?>
                     <p class="noinfo" style="padding-bottom:15px;">
                         <i class="fa fa-user-times fa-2x green" aria-hidden="true"></i>
                         <br>В ближайшее время <?= $username ?> <br class="hidden-xs">не участвует ни в одной игре
                     </p>
+                    <?php 
+                    }
+                    ?>
             </div>
         </div>
 
